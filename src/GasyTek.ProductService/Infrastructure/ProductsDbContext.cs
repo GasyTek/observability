19f131399a25d67c9fef0b60962ab4a20f9b791e
Riana RAMBONIMANANA <rrambonimanana@veepee.com>
2023-03-30 15:17:19 +0200

    Set up product service with seed data and endpoint to get list of products

---
+++ src/GasyTek.ProductService/Infrastructure/ProductsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GasyTek.ProductService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace GasyTek.ProductService.Infrastructure
+{
+    public class ProductsDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=products;Username=postgres;Password=postgres");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed data
+            var fileContent = File.ReadAllText("Data/products.json");
+            var products = JsonConvert.DeserializeObject<Product[]>(fileContent);
+            if (products is not null)
+            {
+                foreach (var product in products)
+                {
+                    modelBuilder.Entity<Product>().HasData(product);
+                }
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- src/GasyTek.ProductService/Program.cs
+++ src/GasyTek.ProductService/Program.cs
@@ -1,41 +1,29 @@
+using GasyTek.ProductService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// EF Core
+builder.Services.AddDbContext<ProductsDbContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Process to auto migration
+var dbContext = new ProductsDbContext();
+dbContext.Database.Migrate();
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/products", (ProductsDbContext dbContext) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return dbContext.Products.ToListAsync();
 })
-.WithName("GetWeatherForecast");
+.WithName("GetProductList");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
