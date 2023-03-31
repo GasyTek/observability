6de3b67205ccecbd00a580c2c097b8cd981eca07
Riana RAMBONIMANANA <rrambonimanana@veepee.com>
2023-03-31 11:31:41 +0200

    Enable Redis on Product Service

---
+++ src/GasyTek.ProductService/Core/CacheExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace GasyTek.ProductService.Core
+{
+    public static class CacheExtensions
+    {
+        public static T Get<T>(this IDistributedCache cache, string key)
+        {
+            var bytes = cache.Get(key);
+            if (bytes == null || bytes.Length == 0)
+            {
+                return default;
+            }
+
+            try
+            {
+                var strValue = Encoding.UTF8.GetString(bytes);
+                return JsonConvert.DeserializeObject<T>(strValue);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        public static void Set<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions options = default)
+        {
+            if(options is null)
+            {
+                options = new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+                    SlidingExpiration = TimeSpan.FromSeconds(30),
+                };
+            }
+
+            var strValue = JsonConvert.SerializeObject(value);
+            var bytes = Encoding.UTF8.GetBytes(strValue);
+            cache.Set(key, bytes, options);
+        }
+    }
+}
--- src/GasyTek.ProductService/Program.cs
+++ src/GasyTek.ProductService/Program.cs
@@ -5,6 +5,9 @@ using Npgsql;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using GasyTek.ProductService.Domain;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,14 @@ builder.Services.AddSwaggerGen();
 // EF Core
 builder.Services.AddDbContext<ProductsDbContext>();
 
+// Redis
+var redisConnection = ConnectionMultiplexer.Connect("localhost:6379");
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.InstanceName = "RedisCache";
+    options.ConnectionMultiplexerFactory = () => Task.FromResult((IConnectionMultiplexer)redisConnection);
+});
+
 // OpenTelemetry
 builder.Services.AddOpenTelemetry()
     .WithTracing(tracerProviderBuilder =>
@@ -23,6 +34,7 @@ builder.Services.AddOpenTelemetry()
             .AddNpgsql()
             .ConfigureResource(resource => resource.AddService(DiagnosticsConfig.ServiceName))
             .AddAspNetCoreInstrumentation()
+            .AddRedisInstrumentation(redisConnection, options => options.SetVerboseDatabaseStatements = true)
             .AddConsoleExporter()
             .AddOtlpExporter());
 //.WithMetrics(metricsProviderBuilder =>
@@ -31,6 +43,8 @@ builder.Services.AddOpenTelemetry()
 //            .AddService(DiagnosticsConfig.ServiceName))
 //        .AddAspNetCoreInstrumentation()
 //        .AddConsoleExporter());
+
+
 
 var app = builder.Build();
 
@@ -47,10 +61,24 @@ app.MapGet("/products", (ProductsDbContext dbContext) =>
     return dbContext.Products.ToListAsync();
 });
 
-app.MapGet("/products/{id:int}", object ([FromRoute] int id, [FromServices] ProductsDbContext dbContext) =>
+app.MapGet("/products/{id:int}", object ([FromRoute] int id, [FromServices] ProductsDbContext dbContext, [FromServices] IDistributedCache cache) =>
     {
-        var product = dbContext.Products.SingleOrDefault(it => it.Id == id);
-        return product is null ? Results.NotFound() : product;
+        var cacheKey = $"Product:{id}";
+        var product = cache.Get<Product>(cacheKey);
+        if (product is null)
+        {
+            // Retrieve product from DB
+            product = dbContext.Products.SingleOrDefault(it => it.Id == id);
+
+            // Add product into cache
+            cache.Set(cacheKey, product);
+
+            return product is null ? Results.NotFound() : product;
+        }
+        else
+        {
+            return product;
+        }
     });
 
 app.Run();
